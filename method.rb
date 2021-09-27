43585282764a9d99a92ad6cc2c090ee11e1cb3cd
beneklomstudent <gcab022107@coderacademy.edu.au>
2021-09-28 09:28:24 +1000

    added Rainbow Ruby to character output, made it a required gem

--- method.rb
+++ method.rb
@@ -1,5 +1,6 @@
 require 'mini_magick'                   # Also need a Gemfile
-                                        # and imagemagick installed
+require 'rainbow'
+                                       # and imagemagick installed
 img = MiniMagick::Image.open 'image2.bmp'
 img.resize('128x64')
 pixels = img.get_pixels                 # Convert pixels to [R,G,B][X][Y] dimensional array
@@ -7,18 +8,28 @@ pixels = img.get_pixels                 # Convert pixels to [R,G,B][X][Y] dimens
 pixels.each do |sub_arrayY|             # Iterate from top to bottom
     sub_arrayY.each do |sub_arrayX|     # Iterate from left to right
         case sub_arrayX.sum/3           #  evaluate (R+G+B) divided by 3 
-        in (0..40)                      #  If 0-60, then output #
-            print "#"
-        in (41..80)                    #  If 61-140, then output =
-            print "="
-        in (80..120)                   #  If 141-230, then output .
-            print "."
-        in (121..160)                      #  If 230 or above, then 
-           print "*"
-        in (161..200)
-            print "$"
+        in (0..20)                      #  If 0-60, then output #
+            print Rainbow("#")
+        in (21..40)                    #  If 61-140, then output =
+            print Rainbow("=")
+        in (41..60)                   #  If 141-230, then output .
+            print Rainbow(".")
+        in (61..80)                      #  If 230 or above, then 
+           print Rainbow("*")
+        in (81..100)
+            print Rainbow("$").blue
+        in (101..120)
+            print Rainbow("/").aqua
+        in (121..140)
+            print Rainbow(",").orange
+        in (141..160)
+            print Rainbow("[").green
+        in (161..180)
+            print Rainbow("]").yellow
+        in (181..200)
+            print Rainbow("0")
         in (201..)
-            print " "                   #   output a space
+            print (" ")                   #   output a space
       
         end
     end
