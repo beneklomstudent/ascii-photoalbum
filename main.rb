83f53c01bc4ffdd91794a3b62ceda36fd7625dbb
beneklomstudent <gcab022107@coderacademy.edu.au>
2021-10-03 17:09:34 +1000

    Revert "unhappy with changes"
    This reverts commit c5ccea08e86aaca0b19b67f6f8ab48d827b45a30.
    
    Going back to fix the code

--- main.rb
+++ main.rb
@@ -1,9 +1,52 @@
 require 'mini_magick'                  
 require 'rainbow'
 require 'colormethod'
-p "please input your image filename as placed in the main directory"
-p imagesource
-p img
-p magic_combine
-p turn_into_array
+
+class ImageData
+   attr_accessor :gettext, :downloadimage, :processimage, :arrayout
+def initialize(gettext, downloadimage, processimage, arrayout)
+@gettext = get.chomp
+@downloadimage = MiniMagick::Image.open gettext
+@processimage = magic_combine
+@arrayout = turn_into_array
+end 
 end
+# p "Please input file locations within the root folder"
+# p ImageData.gettext
+# p ImageData.downloadimage
+# p ImageData.processsimage
+# p ImageData.arrayout
+
+
+
+
+
+
+
+  
+
+
+
+
+
+# begin
+    
+# rescue => exception
+#    "Error" 
+# else
+    
+# end
+
+# begin
+    
+# rescue => TypeError
+#    "Image file" 
+# else
+    
+# end
+
+
+# class asciiimage
+#    def initialize imagearray
+#       @imagearray = imagearray
+#    end
