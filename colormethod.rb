07d93b2fd693ba6d1447c11e9e3ea1f3be2001b7
beneklomstudent <gcab022107@coderacademy.edu.au>
2021-10-04 08:16:30 +1000

    cleaned up code, fixed loading

--- colormethod.rb
+++ colormethod.rb
@@ -1,56 +1,56 @@
 
 def img
-img = MiniMagick::Image.open ("images/image.bmp")
-img.resize('64x32!')
-img.fill ('red')
-img.posterize ('2')
-img.fill ('green')
-img.posterize ('2')
-img.fill ('blue')
-img.posterize ('2')
-end
-
-def save_name
-save_name = gets.chomp
-end
-
-def pixels
-pixels = img.get_pixels 
-end       
-
-def menu_query
-@menu_query = gets.chomp
-end
-
-def pixel_output
-pixels.each do |sub_arrayY|             # Iterate from top to bottom
-    sub_arrayY.each do |sub_arrayX|  # Iterate from left to right    
-case sub_arrayX 
-in [0..50,0..50,100..255]                     
-    print Rainbow("#").blue
-in [100..255,0..50,0..50]                    #
-    print Rainbow("=").red
-in [0..50,100..255,0..50]
-    print Rainbow(".").green
-in [0..50, 0..50, 0..50]
-    print Rainbow("4").black
-in [255, 255, 255]
-    print Rainbow("i").white
-in [100..254, 100..254, 100..254]
-    print Rainbow("%").black
-in [200..255, 124..255, 0..50]
-    print Rainbow("^").yellow
-in [0..50, 100..255, 100..255]
-    print Rainbow("@").darkblue
-in [200..255, 0..50, 200..255]
-    print Rainbow("6").pink
-in [100..255, 100..178, 0..50]
-    print Rainbow("#").orange
-else print ("*")                  #   output a space
-
-end
-end
-print "\n"                  
-
-end
-end
+    img = MiniMagick::Image.open ('images/image.bmp')
+    img.resize('64x32!')
+    img.fill ('red')
+    img.posterize ('2')
+    img.fill ('green')
+    img.posterize ('2')
+    img.fill ('blue')
+    img.posterize ('2')
+    end
+     
+    def save_name
+    save_name = gets.chomp
+    end
+     
+    def pixels
+    pixels = img.get_pixels 
+    end       
+     
+    def menu_query
+    @menu_query = gets.chomp
+    end
+     
+    def pixel_output
+    pixels.each do |sub_arrayY|             # Iterate from top to bottom
+        sub_arrayY.each do |sub_arrayX|  # Iterate from left to right    
+    case sub_arrayX 
+    in [0..50,0..50,100..255]                     
+        print Rainbow("#").blue
+    in [100..255,0..50,0..50]                    #
+        print Rainbow("=").red
+    in [0..50,100..255,0..50]
+        print Rainbow(".").green
+    in [0..50, 0..50, 0..50]
+        print Rainbow("4").black
+    in [255, 255, 255]
+        print Rainbow("i").white
+    in [100..254, 100..254, 100..254]
+        print Rainbow("%").black
+    in [200..255, 124..255, 0..50]
+        print Rainbow("^").yellow
+    in [0..50, 100..255, 100..255]
+        print Rainbow("@").darkblue
+    in [200..255, 0..50, 200..255]
+        print Rainbow("6").pink
+    in [100..255, 100..178, 0..50]
+        print Rainbow("#").orange
+    else print ("*")                  #   output a space
+     
+    end
+    end
+    print "\n"                  
+     
+    end
+    end
--- main.rb
+++ main.rb
@@ -2,29 +2,38 @@ require 'mini_magick'
 require 'rainbow'
 require 'yaml'
 load 'colormethod.rb'
+ 
+ 
 p "Welcome to the Ascii Photolab, with this application you can
 create ascii art from your own images!"
-p "Would you like to create or load an image??"
-p "create"
-p "load"
-
-if menu_query.include? "create" 
-puts "Loading image from file directory #{img}"
-pixels 
-pixel_output 
-puts "Would you like to save your image?"
-puts "yes/no"
-saveit = gets.chomp
-if saveit.include? 'yes'
-File.open("savesimages.yml", "w") { |file| file.write(pixels.to_yaml) } 
-print "image array saved to savesimages.yml"
-if saveit.include? 'no'
-    print "Thanks for using Ascii Photolab!"
-end
-if menu_query.include? 'no'
-pixels_array = YAML.load(File.read("savesimages.yml"))
-pixel_output
-elseif puts "error reading input, please try again"
+ 
+continue = true
+while continue == true
+    p "Would you like to create or load an image??"
+    p "create"
+    p "load"
+    answer = menu_query
+    if answer.include? "create" 
+ 
+        puts "Loading image from file directory #{img}"
+        pixels
+        pixel_output
+        puts "Would you like to save your image?"
+        puts "yes/no"
+        saveit = gets.chomp
+        if saveit.include? 'yes'
+            File.open("savesimages.yml", "w") { |file| file.write(pixels.to_yaml) } 
+            puts "image array saved to savesimages.yml"
+        elsif saveit.include? 'no'
+            puts "Thanks for using Ascii Photolab!"
+            continue = false
+        end
+    elsif answer.include? "load"
+        pixels_array = YAML.load(File.read("savesimages.yml"))
+        img = pixels_array
+        pixel_output
+    else
+        puts "error reading input, please try again"
     end
 end
-end
+ 
