cab80db08bb919759197a9e9911f0b044e258b33
beneklomstudent <gcab022107@coderacademy.edu.au>
2021-10-04 13:52:08 +1000

    Update test1.rb

--- test1.rb
+++ test1.rb
@@ -1,8 +1,19 @@
 require 'mini_magick'                  
 require 'rainbow'                   #Color values can be within the range of 0-255
-load 'colormethod.rb'                 #this test checks the created array against that, if a value goes
-img                                 #over said amount it returns a message
-pixels
+                 #this test checks the created array against that, if a value goes
+img = MiniMagick::Image.open ('images/image.bmp')
+    img.combine_options do |i|
+        i.resize('64x32!')
+        i.fill ('red')
+        i.posterize ('2')
+        i.fill ('green')
+        i.posterize ('2')
+        i.fill ('blue')
+        i.posterize ('2')     
+                                 #over said amount it returns a message
+         def pixels(i)
+        i.get_pixels 
+        end       
 pixels.each do |sub_arrayY|            
     sub_arrayY.each do |sub_arrayX|  
 case sub_arrayX 
@@ -11,5 +22,6 @@ in [256..,256..,256..]
 else return                             #if no error is found code is stopped
 end
 
+end
 end
 end
